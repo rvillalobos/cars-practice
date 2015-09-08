cb16e4df1163a9c116055865afc91d319967c5b5
AldoRuizPimentel <is692909@iteso.mx>
2015-09-08 17:04:24 -0500

    First GasTankTest

---
+++ src/test/java/com/iteso/utpractice/gastanks/FortyLitersTest.java
@@ -0,0 +1,47 @@
+package com.iteso.utpractice.gastanks;
+
+import com.iteso.utpractice.gastanks.impl.FortyLiters;
+import static org.junit.Assert.*;
+import org.junit.*;
+
+/**
+ * Created by Aldo on 08/09/2015.
+ */
+public class FortyLitersTest {
+
+    FortyLiters myGasTank;
+
+    @Before
+    public void setUp()
+    {
+        myGasTank = new FortyLiters();
+    }
+
+    @Test
+    public void testRegularAddGas(){
+
+        assert
+
+    }
+
+    @Test
+    public void testOverflowGasAdd(){
+
+    }
+
+    @Test
+    public void testZeroLiters()
+    {
+
+    }
+
+    @Test
+    public void testGasLevelHigherThanGasCapacity(){
+
+
+    }
+
+
+
+
+}
---
+++ src/test/java/com/iteso/utpractice/gastanks/SixtyLitersTest.java
@@ -0,0 +1,7 @@
+package com.iteso.utpractice.gastanks;
+
+/**
+ * Created by Aldo on 08/09/2015.
+ */
+public class SixtyLitersTest {
+}
