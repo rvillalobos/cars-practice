b2247be454f970c42af082ddf1f34fac32a8b068
AldoRuizPimentel <is692909@iteso.mx>
2015-09-08 17:39:54 -0500

    Finished Tests for GasTanks

--- src/test/java/com/iteso/utpractice/gastanks/FortyLitersTest.java
+++ src/test/java/com/iteso/utpractice/gastanks/FortyLitersTest.java
@@ -1,47 +1,65 @@
 package com.iteso.utpractice.gastanks;
-
 import com.iteso.utpractice.gastanks.impl.FortyLiters;
-import static org.junit.Assert.*;
-import org.junit.*;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Aldo on 08/09/2015.
  */
-public class FortyLitersTest {
+public class FortyLitersTest
+{
 
     FortyLiters myGasTank;
+    double finalGas;
 
     @Before
     public void setUp()
     {
         myGasTank = new FortyLiters();
+        myGasTank.setGasLevel(10);
     }
 
     @Test
-    public void testRegularAddGas(){
-
-        assert
-
+    public void testRegularAddGas()
+    {
+        myGasTank.addGass(10);
+        finalGas = myGasTank.getGasLevel();
+        assertEquals(20, finalGas, 0.001);
     }
 
-    @Test
-    public void testOverflowGasAdd(){
 
+    @Test
+    public void testOverflowGasAdd()
+    {
+        myGasTank.setGasLevel(30);
+        myGasTank.addGass(15);
+        finalGas = myGasTank.getGasLevel();
+        assertEquals(10, finalGas, 0.001);
     }
 
     @Test
     public void testZeroLiters()
     {
-
+        myGasTank.addGass(0);
+        finalGas = myGasTank.getGasLevel();
+        assertEquals(10, finalGas, 0.001);
     }
 
     @Test
-    public void testGasLevelHigherThanGasCapacity(){
-
-
+    public void testMinusLiters()
+    {
+        myGasTank.addGass(-5);
+        finalGas = myGasTank.getGasLevel();
+        assertEquals(10, finalGas, 0.001);
     }
 
-
-
-
+    @Test
+    public void testGasLevelHigherThanGasCapacity()
+    {
+        myGasTank.setGasLevel(41);
+        myGasTank.addGass(1);
+        finalGas = myGasTank.getGasLevel();
+        assertEquals(41, finalGas, 0.001);
+    }
 }
--- src/test/java/com/iteso/utpractice/gastanks/SixtyLitersTest.java
+++ src/test/java/com/iteso/utpractice/gastanks/SixtyLitersTest.java
@@ -1,7 +1,66 @@
 package com.iteso.utpractice.gastanks;
+import com.iteso.utpractice.gastanks.impl.FortyLiters;
+import com.iteso.utpractice.gastanks.impl.SixtyLiters;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Aldo on 08/09/2015.
  */
-public class SixtyLitersTest {
+public class SixtyLitersTest
+{
+
+    SixtyLiters myGasTank;
+    double finalGas;
+
+    @Before
+    public void setUp()
+    {
+        myGasTank = new SixtyLiters();
+        myGasTank.setGasLevel(10);
+    }
+
+    @Test
+    public void testRegularAddGas()
+    {
+        myGasTank.addGass(10);
+        finalGas = myGasTank.getGasLevel();
+        assertEquals(20, finalGas, 0.001);
+    }
+
+
+    @Test
+    public void testOverflowGasAdd()
+    {
+        myGasTank.setGasLevel(50);
+        myGasTank.addGass(15);
+        finalGas = myGasTank.getGasLevel();
+        assertEquals(10, finalGas, 0.001);
+    }
+
+    @Test
+    public void testZeroLiters()
+    {
+        myGasTank.addGass(0);
+        finalGas = myGasTank.getGasLevel();
+        assertEquals(10, finalGas, 0.001);
+    }
+
+    @Test
+    public void testMinusLiters()
+    {
+        myGasTank.addGass(-5);
+        finalGas = myGasTank.getGasLevel();
+        assertEquals(10, finalGas, 0.001);
+    }
+
+    @Test
+    public void testGasLevelHigherThanGasCapacity()
+    {
+        myGasTank.setGasLevel(61);
+        myGasTank.addGass(1);
+        finalGas = myGasTank.getGasLevel();
+        assertEquals(61, finalGas, 0.001);
+    }
 }
