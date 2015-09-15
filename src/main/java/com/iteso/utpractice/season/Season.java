649e26e8122cbc7009f605f697aa54b96f649b96
IS682431 <IS682431@T1060307.ds.iteso.mx>
2015-09-15 08:26:02 -0500

    test from iteso

--- src/main/java/com/iteso/utpractice/season/Season.java
+++ src/main/java/com/iteso/utpractice/season/Season.java
@@ -5,6 +5,10 @@ package com.iteso.utpractice.season;
  */
 public class Season {
     public String whichSeason(int day, int month) {
+    	if(month <= 0 || month >= 32)
+    		return "invalid";
+    	if (month % 1 != 0)
+    		return "invalid";
         return null;
     }
 }
