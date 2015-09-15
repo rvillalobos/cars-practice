2185ba448ea2ddc3a7f0a8c507abf14d0ec9d011
Geovani Serrano <GeovaniSerrano@MacBook-Pro-de-Geovani.local>
2015-09-15 08:05:10 -0500

    Today and Season class done

--- src/main/java/com/iteso/utpractice/season/Season.java
+++ src/main/java/com/iteso/utpractice/season/Season.java
@@ -5,6 +5,101 @@ package com.iteso.utpractice.season;
  */
 public class Season {
     public String whichSeason(int day, int month) {
-        return null;
+        if((day >= 1 && month >= 1) || (day <= 31 && month <= 12)) {
+            switch (month) {
+                case 1:
+                    if(day <= 31 && day >= 21){
+                        return "Winter";
+                    } else {
+                        return "Invalid";
+                    }
+                case 2:
+                    if(day <= 28) {
+                        return "Winter";
+                    } else {
+                        return "Invalid";
+                    }
+                case 3:
+                    if(day < 21) {
+                        return "Winter";
+                    } else {
+                        if(day <= 31 && day >= 21){
+                            return "Spring";
+                        } else {
+                            return "Invalid";
+                        }
+                    }
+                case 4:
+                    if(day <= 30) {
+                        return "Spring";
+                    } else {
+                        return "Invalid";
+                    }
+                case 5:
+                    if (day <= 31) {
+                        return "Spring";
+                    } else {
+                        return "Invalid";
+                    }
+                case 6:
+                    if(day < 21) {
+                        return "Spring";
+                    } else {
+                        if(day <= 30 && day >= 21){
+                            return "Summer";
+                        } else {
+                            return "Invalid";
+                        }
+                    }
+                case 7:
+                    if (day <= 31) {
+                        return "Summer";
+                    } else {
+                        return "Invalid";
+                    }
+                case 8:
+                    if (day <= 31) {
+                        return "Summer";
+                    } else {
+                        return "Invalid";
+                    }
+                case 9:
+                    if(day < 21) {
+                        return "Summer";
+                    } else {
+                        if(day <= 30 && day >= 21){
+                            return "Autumn";
+                        } else {
+                            return "Invalid";
+                        }
+                    }
+                case 10:
+                    if (day <= 31) {
+                        return "Autumn";
+                    } else {
+                        return "Invalid";
+                    }
+                case 11:
+                    if (day <= 30) {
+                        return "Autumn";
+                    } else {
+                        return "Invalid";
+                    }
+                case 12:
+                    if(day < 21) {
+                        return "Autumn";
+                    } else {
+                        if(day <= 30 && day >= 21){
+                            return "Winter";
+                        } else {
+                            return "Invalid";
+                        }
+                    }
+                default:
+                    return "Invalid";
+            }
+        } else {
+            return "Invalid";
+        }
     }
 }
--- src/main/java/com/iteso/utpractice/today/Today.java
+++ src/main/java/com/iteso/utpractice/today/Today.java
@@ -1,10 +1,27 @@
 package com.iteso.utpractice.today;
 
+import java.util.Calendar;
+
 /**
  * Created by rvillalobos on 9/15/14.
  */
 public class Today {
     public boolean isToday(int day) {
-        return false;
+
+        Calendar myCalendar;
+        int today;
+
+        myCalendar = Calendar.getInstance();
+        today = myCalendar.get(Calendar.DAY_OF_WEEK);
+
+        if(day < 1 && day > 7) {
+            return false;
+        } else {
+            if(today == day){
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
 }
