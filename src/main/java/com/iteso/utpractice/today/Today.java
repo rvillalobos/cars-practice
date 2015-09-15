5e5fc28aeb21deec9d59d8ed13a7b321715d1f23
GeraCruz <nash.179.1@gmail.com>
2015-09-15 08:07:27 -0500

    Chino

--- src/main/java/com/iteso/utpractice/today/Today.java
+++ src/main/java/com/iteso/utpractice/today/Today.java
@@ -1,10 +1,18 @@
 package com.iteso.utpractice.today;
 
+import java.util.Calendar;
+
 /**
  * Created by rvillalobos on 9/15/14.
  */
 public class Today {
     public boolean isToday(int day) {
-        return false;
+       if(day >= 1 && day <= 7)
+           if(day == Calendar.getInstance().get(Calendar.DAY_OF_WEEK))
+               return true;
+            else
+               return false;
+        else
+           return false;
     }
 }
